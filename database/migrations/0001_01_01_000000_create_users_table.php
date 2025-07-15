bbab14ea8d43f65f8996525884c655e3912ac438
muhammedkusay <mhmadhamado35@gmail.com>
2025-07-15 07:24:57 +0300

    Modified timestamps()

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -27,7 +27,7 @@ return new class extends Migration
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
-            $table->timestamps()->nullable();
+            $table->timestamps();
         });
 
         Schema::create('sessions', function (Blueprint $table) {
